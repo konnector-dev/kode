8afbc11d7c378e804bfa509d71094276bb74e999
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-03-15 14:24:34 +0530

    Add LinkedIn and WordPress links, remove outline from theme-change-icons

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -31,8 +31,9 @@ use Cake\Routing\Router;
     class="min-h-screen bg-black flex justify-center py-10 sm:px-6 lg:px-8"
     v-bind:class="{'bg-white': isLight}">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <img class="mx-auto h-24 w-auto" src="<?php echo Router::url('/img/koreview.png'); ?>"
-             alt="<?php echo env('APP_NAME'); ?>"/>
+        <a href="<?php echo Router::url('/'); ?>"><img class="mx-auto h-24 w-auto"
+                                                       src="<?php echo Router::url('/img/koreview.png'); ?>"
+                                                       alt="<?php echo env('APP_NAME'); ?>"/></a>
         <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-600">
             <?php echo env('APP_NAME'); ?>
         </h2>
@@ -42,17 +43,29 @@ use Cake\Routing\Router;
                 </span>
         </div>
         <div class="text-gray-600 flex justify-center">
-            <div class="flex-1 text-center"><a href="https://twitter.com/jdecode" alt="jdecode at Twitter"
-                                               target="_blank"><i
-                        class="fa fa-twitter"></i></a>/<a href="https://github.com/jdecode" alt="jdecode at GitHub"
-                                                          target="_blank"><i
-                        class="fa fa-github"></i></a> jdecode(<a href="https://jdecode.dev"
-                                                                 alt="jdecode's blog" target="_blank">.dev</a>)
+            <div class="flex-1 text-center text-xl">
+                <a href="https://twitter.com/jdecode" alt="jdecode at Twitter" target="_blank">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a href="https://github.com/jdecode" alt="jdecode at GitHub"
+                                                          target="_blank">
+                    <i class="fa fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/jdecode" alt="jdecode at LinkedIn"
+                                                          target="_blank">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+                <a href="https://jdecode.dev" alt="jdecode at LinkedIn"
+                                                          target="_blank">
+                    <i class="fa fa-wordpress"></i>
+                </a>
             </div>
-            <div class="flex-1 text-center"><a href="<?php echo Router::url('/'); ?>">Home</a> / <a
-                    href="<?php echo Router::url('/login'); ?>">Login</a></div>
+            <div class="flex-1 text-center"><a
+                    href="<?php echo Router::url('/login'); ?>">Sign in</a></div>
             <div class="flex-1 text-center">
-                <button class="theme" v-on:click="themeUpdate"><i class="fa fa-moon-o"></i>/<i class="fa fa-sun-o"></i></button>
+                <button class="theme focus:outline-none" v-on:click="themeUpdate" title="Toggle Light/Dark mode">
+                    <i class="fa fa-moon-o"></i>/<i class="fa fa-sun-o"></i>
+                </button>
             </div>
         </div>
     </div>
