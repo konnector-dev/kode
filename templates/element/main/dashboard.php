485382c6727fd582b49e9561fad74f7e5dfc43e7
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-03-31 03:44:41 +0530

    Set main element with ID as hash, to be used soon

--- templates/element/main/dashboard.php
+++ templates/element/main/dashboard.php
@@ -1,4 +1,4 @@
-<main class="flex-1 relative z-0 overflow-y-auto py-6 focus:outline-none" tabindex="0" x-data x-init="$el.focus()">
+<main id="dash" class="flex-1 relative z-0 overflow-y-auto py-6 focus:outline-none" tabindex="0">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
         <h1 class="text-2xl font-semibold text-gray-600">Dashboard</h1>
     </div>
