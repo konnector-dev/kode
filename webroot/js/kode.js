79c13f734a768d749678f16fe4382bb79ee2b839
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-03-31 03:43:33 +0530

    Add token

--- webroot/js/kode.js
+++ webroot/js/kode.js
@@ -3,12 +3,14 @@ new Vue({
     el: '.darkLight',
     data: {
         isDark: false,
-        showUserOptions: false
+        showUserOptions: false,
+        token: ''
     },
     mounted() {
         if (localStorage.isDark) {
             this.isDark = JSON.parse(localStorage.isDark);
         }
+        this.token = window.token;
     },
     methods: {
         darkLightModeUpdate: function() {
