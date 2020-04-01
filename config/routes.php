95d8ffb162519c06638d5349811ebc2edd7f68be
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-04-02 04:20:25 +0530

    Add route for front-end app

--- config/routes.php
+++ config/routes.php
@@ -71,6 +71,7 @@ $routes->scope('/', function (RouteBuilder $builder) {
      * ...and connect the rest of 'Pages' controller's URLs.
      */
     $builder->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    $builder->connect('/app/*', ['controller' => 'Users', 'action' => 'frontend']);
 
     /*
      * Connect catchall routes for all controllers.
