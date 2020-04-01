2b5fb59ecccda1cd0dade53c3f43e863bcff8e81
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-04-02 04:24:53 +0530

    Add vue-in-cake template files

---
+++ templates/layout/frontend.php
@@ -0,0 +1,34 @@
+<?php
+
+use Cake\Core\Configure;
+use Cake\Routing\Router;
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo Configure::read('App.defaultLocale'); ?>">
+
+<head>
+    <meta charset="utf-8">
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo Configure::read('app_name'); ?></title>
+
+    <link rel="icon" type="image/png" href="/favicon.png"/>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="<?php echo Router::url('/fonts/font-awesome.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo Router::url('/css/style.css'); ?>" rel="stylesheet">
+
+</head>
+
+<body>
+<div id="app">
+
+</div>
+<script src="<?php echo Router::url('/js/vuedev/dist/js/app.js') ?>"></script>
+<script src="<?php echo Router::url('/js/vuedev/dist/js/chunk-vendors.js') ?>"></script>
+</body>
+
+</html>
