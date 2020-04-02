d2982b34ecd5fed33b4b7adadd5eabdd534560c4
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-04-02 20:17:50 +0530

    Route dashboard to root

--- webroot/vuedev/src/routes.js
+++ webroot/vuedev/src/routes.js
@@ -11,7 +11,7 @@ export default {
         },
         {
             path: '/dashboard',
-            component: Dashboard
+            redirect: '/'
         },
         {
             path: '/projects',
