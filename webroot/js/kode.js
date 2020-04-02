5de663b47dc8474dd7a5bb7d8dba19f57d482d57
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-04-02 20:13:43 +0530

    Move userOptions/token to Vue

--- webroot/js/kode.js
+++ webroot/js/kode.js
@@ -2,23 +2,17 @@
 new Vue({
     el: '.darkLight',
     data: {
-        isDark: false,
-        showUserOptions: false,
-        token: ''
+        isDark: false
     },
     mounted() {
         if (localStorage.isDark) {
             this.isDark = JSON.parse(localStorage.isDark);
         }
-        this.token = window.token;
     },
     methods: {
         darkLightModeUpdate: function() {
             this.isDark = !this.isDark;
             localStorage.isDark = this.isDark;
-        },
-        toggleUserOptions: function () {
-            this.showUserOptions = !this.showUserOptions;
         }
-    },
+    }
 });
