b952f380a0afaeb6f748306e19e08e437644fceb
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-04-02 20:29:03 +0530

    Use router to get token from query params; use component

--- webroot/vuedev/src/main.js
+++ webroot/vuedev/src/main.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router'
 
 import routes from './routes';
 
+import UserSettings from '@/components/UserSettings/UserSettings'
+
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false
@@ -12,23 +14,24 @@ new Vue({
     el: '.darkLight',
     data: {
         isDark: false,
-        showUserOptions: false,
         token: ''
+    },
+    created () {
+        this.token = this.$route.query.token;
     },
     mounted() {
         if (localStorage.isDark) {
             this.isDark = JSON.parse(localStorage.isDark);
         }
-        this.token = window.token;
     },
     methods: {
         darkLightModeUpdate: function() {
             this.isDark = !this.isDark;
             localStorage.isDark = this.isDark;
-        },
-        toggleUserOptions: function () {
-            this.showUserOptions = !this.showUserOptions;
         }
+    },
+    components:{
+        UserSettings
     },
     router: new VueRouter(routes)
 });
