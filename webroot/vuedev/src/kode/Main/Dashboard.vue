c64811caf0b191ea67b6a0d947720d568d28eb11
JD Singh <jdecode@gmail.com>
2020-04-22 20:01:01 +0530

    Move everything of vue to root

--- webroot/vuedev/src/kode/Main/Dashboard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <main id="dash" class="flex-1 relative z-0 overflow-y-auto py-6 focus:outline-none" tabindex="0">
-        <router-view></router-view>
-    </main>
-</template>
-<script>
-    export default {};
-</script>
--- webroot/vuedev/vue.config.js
+++
@@ -1,8 +0,0 @@
-let path = require('path');
-
-module.exports = {
-    publicPath: '',
-    chainWebpack: config => {
-        config.resolve.alias.set('vue$', 'vue/dist/vue.common.js');
-    },
-}
